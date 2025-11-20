821a8d51c38cfa7ffa33837df2273512c525795b
Snofts <samuelsnofts@gmail.com>
2025-11-20 16:25:19 +0100

    Remain footer and contact section to be included

---
+++ src/pages/Home.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import ThemeToggle from '../components/ThemeToggle'
+import StarBackground from '../components/StarBackground'
+import NavBar from '../components/NavBar'
+import HeroSection from '../components/HeroSection'
+import AboutSection from '../components/AboutSection'
+import SkilsSection from '../components/SkilsSection'
+import ProjectSection from '../components/ProjectSection'
+import ContactSection from '../components/ContactSection'
+
+const Home = () => {
+  return (
+    <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
+        {/* Theme toggle */}
+        <ThemeToggle />
+
+        {/* Background Effects */}
+        <StarBackground />
+
+        {/* NavBar */}
+        <NavBar />
+
+        {/* MainContent */}
+        <main>
+          <HeroSection />
+          <AboutSection />
+          <SkilsSection />
+          <ProjectSection />
+          <ContactSection />
+        </main>
+
+        {/* Footer */}
+    </div>
+  )
+}
+
+export default Home
